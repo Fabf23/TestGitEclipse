b70f2c8d163e8f4a52fd107cad99710bb456a175
Fabf23 <fabien.mazenq@gmail.com>
2020-12-08 11:05:43 +0100

    Premier commit

---
+++ TestGit/src/Test/TestClasse.java
@@ -0,0 +1,14 @@
+/*
+ * TestClasse.java                                                          8 déc. 2020
+ * IUT info1 2017-2018 groupe 3, ni copyright ni copyleft 
+ */
+package Test;
+
+/**
+ * TODO commenter les responsabilités de cette classe
+ * @author f.mazenq
+ *
+ */
+public class TestClasse {
+
+}
